cf5ead260d2ad0676c6492bcbdcd61d3cf14aaf1
Austin Raney <aaraney@crimson.ua.edu>
2020-04-06 09:28:23 -0500

    Updated parser to find latest table data

--- scripts/parse_datawrapper.py
+++ scripts/parse_datawrapper.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 
 import requests
+import re
+from os.path import join
 from bs4 import BeautifulSoup as bs
 
 # GoodRx `Where Can I Get a Drive-Thru Coronavirus (Covid-19) Test Near Me?`
 # embeded table url
-data_url = 'https://datawrapper.dwcdn.net/H7PJn/6/'
+base_url = 'https://datawrapper.dwcdn.net/'
+search_url = 'https://datawrapper.dwcdn.net/H7PJn/'
 
-site_data = requests.get(data_url)
+pre_scrape = requests.get(search_url)
 
-soup = bs(site_data.text, 'html.parser')
+soup = bs(pre_scrape.text, 'html.parser')
+
+table_location_suffix = re.search('/H7PJn.*', soup.find('meta').attrs['content']).group().lstrip('/')
+
+url = join(base_url, table_location_suffix)
+
+res = requests.get(url)
+
+soup = bs(res.text, 'html.parser')
 
 # Parse javascript function containing drive thru location data
 raw_table = soup.find_all('script')[-1]
